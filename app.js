815bfd8f5aba1f6096c63add711d2c8728ff9f14
Robert Schmahl <rschm007@gmail.com>
2020-12-09 18:55:07 -0800

    created db and added app.js with inquirer functionality. Need to test

---
+++ app.js
@@ -0,0 +1,175 @@
+// ==================================================
+// CREATE A CONNECTION
+// ==================================================
+
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "bPT#HrSYFtnR",
+  database: "employee_trackerDB",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  runApp();
+});
+
+// ==================================================
+// BEGIN WORKING CODE
+// ==================================================
+
+function runApp() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "Department Options",
+        "Role Options",
+        "Employee Options",
+        "End",
+      ],
+    })
+    // ***** Department Options
+    .then((answer) => {
+      switch (answer.action) {
+        
+        case "Department Options":
+          return inquirer
+            .prompt({
+              name: "department_options",
+              type: "rawlist",
+              message: "DEPARTMENTS:  What would you like to do?",
+              choices: [
+                "View a department",
+                "View all departments",
+                "Add a department",
+                "Delete a department",
+                "View the total utilized budget of a department",
+                "Go back",
+              ],
+              // *** department options logic
+            })
+            .then((answer) => {
+              switch (answer.department_options) {
+                case "View a department":
+                  break;
+
+                case "View all departments":
+                  break;
+
+                case "Add a department":
+                  break;
+
+                case "Delete a department":
+                  break;
+
+                case "View the total utilized budget of a department":
+                  break;
+
+                case "Go back":
+                  runApp();
+                  break;
+              }
+            });
+
+        // ***** Department Options
+
+        case "Role Options":
+          return inquirer
+            .prompt({
+              name: "role_options",
+              type: "rawlist",
+              message: "ROLES:  What would you like to do?",
+              choices: [
+                "View all roles",
+                "View a role",
+                "Add a role",
+                "Delete a role",
+                "Go back",
+              ],
+              // *** role options logic
+            })
+            .then((answer) => {
+              switch (answer.role_options) {
+                case "View a role":
+                  break;
+
+                case "View all roles":
+                  break;
+
+                case "Add a role":
+                  break;
+
+                case "Delete a role":
+                  break;
+
+                case "Go back":
+                  runApp();
+                  break;
+              }
+            });
+
+        case "Employee Options":
+          return inquirer
+            .prompt({
+              name: "employee_options",
+              type: "rawlist",
+              message: "EMPLOYEES:  What would you like to do?",
+              choices: [
+                "View an employee",
+                "View all employees",
+                "View employees by manager",
+                "Update employee managers",
+                "Add an employee",
+                "Update an employee",
+                "Delete an employee",
+                "Go back",
+              ],
+              // *** role options logic
+            })
+            .then((answer) => {
+              switch (answer.role_options) {
+                case "View an employee":
+                  break;
+
+                case "View all employees":
+                  break;
+
+                case "View employees by manager":
+                  break;
+
+                case "Update employee managers":
+                  break;
+
+                case "Add an employee":
+                  break;
+
+                case "Update an employee":
+                  break;
+
+                case "Delete an employee":
+                  break;
+
+                case "Go back":
+                  runApp();
+                  break;
+              }
+            });
+
+        case "End":
+          break;
+      }
+    });
+}
---
+++ schema.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+
+CREATE DATABASE employee_trackerDB;
+
+USE employee_trackerDB;
+
+-- department table
+CREATE TABLE department (
+id INT NOT NULL AUTO_INCREMENT,
+name VARCHAR(30),
+PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+id INT NOT NULL AUTO_INCREMENT,
+title VARCHAR(30),
+salary DECIMAL,
+department_id INT,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INT,
+manager_id INT,
+PRIMARY KEY (id)
+);
+
+-- Selecting
+SELECT * FROM department;
+
+SELECT * FROM role;
+ 
+SELECT * FROM employee;
